28a24e281b55b5b621606bb86bc8dcc2d3c3334e
Danipro942 <45660528+Danipro942@users.noreply.github.com>
2025-06-30 08:02:50 -0400

    Changing logo and pic

--- src/Components/Aboutme/AnimatedPortrait/index.tsx
+++ src/Components/Aboutme/AnimatedPortrait/index.tsx
@@ -7,7 +7,7 @@ type PortraitProps = {
 };
 
 const AnimatedPortrait = ({
-  imageSrc,
+  // imageSrc,
   altText = "Foto de perfil",
   size = "16px", // Valor por defecto (1rem = 16px)
 }: PortraitProps) => {
@@ -18,7 +18,11 @@ const AnimatedPortrait = ({
 
   return (
     <div className={styles.portraitContainer} style={componentStyles}>
-      <img src={imageSrc} alt={altText} className={styles.portraitImage} />
+      <img
+        src={`https://media.licdn.com/dms/image/v2/D5603AQFx2QNDlYORDw/profile-displayphoto-crop_800_800/B56ZfAr9bgHEAI-/0/1751284439682?e=1756944000&v=beta&t=SgeoVJlAJAGX1q2CNt8WnfspWU3YnTJ5JnNEiQ5HCPo`}
+        alt={altText}
+        className={styles.portraitImage}
+      />
     </div>
   );
 };
